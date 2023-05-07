387a1971dd43b7b9cbcc250d503791e2248843e9
Andrew Kal <andrewkalgit@gmail.com>
2023-05-06 21:06:58 -0400

    demo initialize and lay out mainly in html

--- src/Demo.js
+++ src/Demo.js
@@ -11,9 +11,25 @@ function Demo(props) {
                 Disclaimer: This is for demo purposes only! Please Do NOT put in sensitive information.
             </h2>
             <br></br>
-            <div>
-                
-            </div>
+            <main>
+                <div>
+                <h3>
+                    Login
+                </h3>
+                  <button> Guest Login </button>
+                </div>
+                <div>
+                <h3> Browsing Items </h3>
+                <button> Add to Cart </button>
+                <button> Remove from Cart </button>
+                </div>
+                <div>
+                <h3>
+                Check out (Buying)
+                </h3>
+                <button> Check out </button>
+                </div>
+            </main>
         </div>
         </>
     );
