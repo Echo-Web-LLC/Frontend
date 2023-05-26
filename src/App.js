bde20c4101cdb964689ded63dabda0d347a4054c
Sungmin Lee <sungminlee417@gmail.com>
2023-05-25 17:45:49 -0700

    attempting to add glow to sphere

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import Nav from './Nav'
-import Landing from './Landing'
-import Demo from './Demo'
-import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Nav from "./Nav";
+import Landing from "./Landing";
+import Demo from "./Demo";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-    <Nav></Nav>
-    <Routes>
-    <Route path="/" element={<Landing></Landing>}></Route>
-    <Route path="/demo" element={<Demo></Demo>}></Route>
-    </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Nav></Nav>
+        <Routes>
+          <Route path="/" element={<Landing></Landing>}></Route>
+          <Route path="/demo" element={<Demo></Demo>}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- src/Landing.js
+++ src/Landing.js
@@ -1,7 +1,12 @@
-import React, { useRef } from 'react';
-import { Canvas, useFrame } from '@react-three/fiber';
-import { PerspectiveCamera, Sphere } from '@react-three/drei';
-import * as THREE from 'three';
+import React, { useRef } from "react";
+import { Canvas, useFrame, extend } from "@react-three/fiber";
+import { PerspectiveCamera, Sphere } from "@react-three/drei";
+import { Effects } from "@react-three/drei";
+import { UnrealBloomPass } from "three-stdlib";
+import * as THREE from "three";
+import Nav from "./Nav";
+
+extend({ UnrealBloomPass });
 
 function GradientSphereMaterial() {
   const ref = useRef();
@@ -23,22 +28,29 @@ function GradientSphereMaterial() {
         }
       `}
       fragmentShader={`
-        uniform float time;
-        varying vec2 vUv;
-        void main() {
-          vec3 colorA = vec3(231.0, 231.0, 231.0) / 255.0;
-          vec3 colorB = vec3(39.0, 39.0, 39.0) / 255.0;
-          vec3 gradient = mix(colorA, colorB, vUv.y);
-          vec2 center = vec2(0.5, 0.5);
-          float radius = 2.0;
-          float glowStrength = 1.0 + 1.0 * sin(1.0 * time);
-          float distanceFromCenter = distance(vUv, center);
-          float glowFactor = smoothstep(radius - 0.1, radius + 0.1, distanceFromCenter);
-          vec3 glowColor = vec3(0, 0, 0); // Set the glow color to white
-          vec4 glow = vec4(glowColor, 1.0) * glowStrength * (1.0 - glowFactor);
-          gl_FragColor = vec4(glow.rgb + gradient.rgb, 1.0);
-        }
-      `}
+      uniform float time;
+      varying vec2 vUv;
+      void main() {
+        vec3 colorA = vec3(231.0, 231.0, 231.0) / 255.0;
+        vec3 colorB = vec3(39.0, 39.0, 39.0) / 255.0;
+        vec3 gradient = mix(colorA, colorB, vUv.y);
+
+        // Add pulsating effect
+        float pulsateStrength = 0.2; // Strength of pulsation effect
+        float pulsateSpeed = 2.0; // Speed of pulsation effect
+        float pulsate = pulsateStrength * sin(pulsateSpeed * time);
+        gradient += pulsate;
+
+        vec2 center = vec2(0.5, 0.5);
+        float radius = 2.0;
+        float glowStrength = 1.0 + 1.0 * sin(1.0 * time);
+        float distanceFromCenter = distance(vUv, center);
+        float glowFactor = smoothstep(radius - 0.1, radius + 0.1, distanceFromCenter);
+        vec3 glowColor = vec3(0, 0, 0); // Set the glow color to black
+        vec4 glow = vec4(glowColor, 1.0) * glowStrength * (1.0 - glowFactor);
+        gl_FragColor = vec4(glow.rgb + gradient.rgb, 1.0);
+      }
+    `}
       uniforms={{
         time: { value: 0.0 },
       }}
@@ -48,24 +60,30 @@ function GradientSphereMaterial() {
 
 function Landing(props) {
   return (
-    <>
-      <div style={{ width: '100%', height: '90vh', position: 'relative' }} className="canvas_3D_container">
-        <Canvas
-          style={{ width: '100%', height: '105%' }}
-          camera={{ position: [0, 0, 20], near: 0.1, far: 100 }}
-          className="canvas"
-          dpr={[1, 2]}
+    <div style={{ width: "100%", height: "90vh", position: "relative" }}>
+      <Canvas
+        shadows
+        style={{ height: "100%" }}
+        camera={{ position: [0, 0, 20], near: 0.1, far: 100 }}
+        className="canvas"
+        dpr={[1, 2]}
+        gl={{ alpha: true }}
+      >
+        <PerspectiveCamera
+          fov={75}
+          aspect={window.innerWidth / window.innerHeight}
+          near={0.1}
+          far={100}
         >
-          <PerspectiveCamera fov={75} aspect={window.innerWidth / window.innerHeight} near={0.1} far={100}>
-            <ambientLight intensity={0.5} />
-            <pointLight position={[10, 10, 10]} />
-          </PerspectiveCamera>
-          <Sphere args={[8, 32, 32]}>
-            <GradientSphereMaterial />
-          </Sphere>
-        </Canvas>
-      </div>
-    </>
+          <ambientLight intensity={0.5} />
+          <pointLight position={[10, 10, 10]} />
+        </PerspectiveCamera>
+        <Effects>
+          <unrealBloomPass />
+        </Effects>
+        <Sphere args={[8, 32, 32]}>{/* <GradientSphereMaterial /> */}</Sphere>
+      </Canvas>
+    </div>
   );
 }
 
