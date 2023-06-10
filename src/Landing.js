5f3d7a27d0ee7a28de8cea45c211f3c44a89b1f0
Andrew Kal <andrewkalgit@gmail.com>
2023-06-10 11:22:33 -0400

    mobile responsiveness

--- src/Landing.js
+++ src/Landing.js
@@ -55,25 +55,50 @@ const Landing = () => {
     </Helmet>
       <title> landing page </title>
       <meta name='description' content='Beginner friendly page for learning React Helmet.' />
-      <section id="fade-in" className="overflow-hidden bg-gray-50 grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-2 ">
+      <section id="fade-in" className="overflow-hidden bg-gray-50 grid grid-cols-1  xl:grid-cols-2 ">
         <div className="p-8 md:p-12 lg:px-16 lg:py-36">
           <div className="mx-auto max-w-lg text-center ltr:sm:text-left rtl:sm:text-right">
-            <h1 className="flex justify-center text-sm md:text-lg lg:text-2xl xl:text-3xl font-bold lg:text-left text-gray-900 text-center">
+            <h1 className="flex justify-center lg:-mx-2 text-sm md:text-lg lg:text-2xl xl:text-3xl font-bold xl:text-left text-gray-900 text-center">
               Unlock the Power of Minimalistic Design and User Experience for Your Business..
             </h1>
 
-            <p className="hidden text-center lg:text-left text-gray-500 md:mt-4 md:block">
+            <p className="hidden text-center lg:-mx-2 xl:text-left text-gray-500 md:mt-4 md:block">
               Experience the difference of our comprehensive services encompassing design, development, maintenance,  and client collaboration today.
             </p>
 
-            <div className="mt-4 md:mt-8 flex justify-center lg:justify-start">
+            {/* <div className="mt-4 md:mt-8 flex justify-center lg:justify-start">
               <a
+                className="inline-block rounded bg-gray-500 px-12 py-3 text-sm font-medium text-white transition hover:bg-gray-400 focus:outline-none focus:ring focus:ring-gray-700"
                 href="https://ixqu3aocu0m.typeform.com/to/WpcyelXK"
-                className="inline-block rounded bg-blue-400 px-12 py-3 text-sm font-medium text-white transition hover:bg-blue-500 focus:outline-none focus:ring focus:ring-indigo-400"
               >
                 Start Today
               </a>
-            </div>
+            </div> */}
+            <a
+            class="group relative xl:right-1/3 inline-block mt-4 rounded bg-gray-400 px-12 py-3 text-sm font-medium text-white transition hover:bg-gray-300 focus:outline-none focus:ring focus:ring-gray-500"
+            href="https://ixqu3aocu0m.typeform.com/to/WpcyelXK"
+          >
+              <span class="absolute -end-full transition-all group-hover:end-4">
+                <svg
+                  class="h-5 w-5 rtl:rotate-180"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M17 8l4 4m0 0l-4 4m4-4H3"
+                  />
+                </svg>
+              </span>
+
+              <span class="text-xs sm:text-sm font-medium transition-all group-hover:me-4">
+                Let's Connect
+              </span>
+            </a>
           </div>
         </div>
 
