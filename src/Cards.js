488f2bf668503f282426d2e9bb543adfbba04222
Andrew Kal <andrewkalgit@gmail.com>
2023-06-22 20:45:47 -0400

    logo update

--- src/Cards.js
+++ src/Cards.js
@@ -13,10 +13,10 @@ const Cards = React.forwardRef((props, ref) => {
         />
   </Helmet>
 <section
-  className="servicesCTASection bg-gray-100"
+  className="servicesCTASection bg-blue-50"
 >
   <div
-    class="absolute inset-0 bg-white/75 sm:bg-transparent sm:from-white/95 sm:to-white/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l"
+    class="absolute inset-0 sm:to-white/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l"
   ></div>
 
   <div
@@ -54,104 +54,6 @@ const Cards = React.forwardRef((props, ref) => {
     </div>
   </div>
 </section>
-{/* <div className="bg-image-opac">
-<div className="bg-bottom pb-32">
-<Helmet>
-      <title> Echo Web, LLC </title>
-      <meta name='Footer' content='Information about services provided by Business'/>
-</Helmet>
-<section ref={ref}>
-    <h1 class="flex justify-center text-5xl lg:text-6xl font-semibold text-gray-50 py-12">
-      Services
-    </h1>
-  <div className="grid grid-cols-1 xl:grid-cols-3 gap-10 px-10 xl:px-40">
-        <article class="overflow-hidden rounded-lg shadow transition hover:shadow-lg">
-  <img
-    alt="Girl Thinking"
-    src={'https://echowebllc.s3.amazonaws.com/Hotpot_(1)-transformed.png'}
-    class="h-56 w-full object-cover"
-  />
-
-  <div class="bg-white p-4 sm:p-6 h-auto 2xl:h-80">
-
-      <h3 class="mt-0.5 text-2xl text-gray-900">
-        Web Design
-      </h3>
-
-    <p class="mt-2 text-xs/relaxed xl:text-sm/relaxed text-gray-500">
-    At Echo Web, LLC, we offer web design services that are a powerful step in the overall process of establishing a compelling online presence. Web design serves as the gateway to your brand, providing a visually captivating and user-friendly platform for engaging with your audience.
-    </p>
-    <ul class="mt-2 text-md/relaxed text-gray-700">
-        <span class="font-bold"> Highlights: </span>
-        <li>✅ Brochure Sites</li>
-        <li>✅ Ecommerce Design</li>
-        <li>✅ Website Redesign</li>
-    </ul>
-  </div>
-</article>
-<article class="overflow-hidden rounded-lg shadow transition hover:shadow-lg">
-  <img
-    alt="A Laptop Computer"
-    src={'https://echowebllc.s3.amazonaws.com/Hotpot_(2)-UBgTZ3CpW-transformed.png'}
-    class="h-56 w-full object-cover"
-  />
-
-  <div class="bg-white p-4 sm:p-6 h-auto 2xl:h-80">
-
-
-      <h3 class="mt-0.5 text-2xl text-gray-900">
-        Web Development
-      </h3>
-
-    <p class="mt-2 text-xs/relaxed xl:text-sm/relaxed text-gray-500">
-      By developing the design ourselves, we can ensure that every aspect of the user experience is carefully executed. This includes optimizing page loading times, implementing responsive design for various devices, and integrating interactive elements that enhance user engagement.
-    </p>
-      <ul class="mt-2 text-md/relaxed text-gray-700">
-        <span class="font-bold"> Highlights: </span>
-        <li>✅ Efficient Rendering </li>
-        <li>✅ Scalability and Performance </li>
-        <li>✅ Mobile Friendly </li>
-    </ul>
-  </div>
-</article>
-<article class="overflow-hidden rounded-lg shadow transition hover:shadow-lg">
-  <img
-    alt="A Laptop Computer"
-    src={'https://echowebllc.s3.amazonaws.com/Hotpot_(4)-transformed.png'}
-    class="h-56 w-full object-cover"
-  />
-
-  <div class="bg-white p-4 h-auto 2xl:h-80">
-
-      <h3 class="mt-0.5 text-2xl text-gray-900">
-        Maintenance
-      </h3>
-
-    <p class="mt-2 text-xs/relaxed xl:text-sm/relaxed text-gray-500">
-      By entrusting us with the maintenance of your website or application, you can have peace of mind knowing that it is in capable hands. We understand the intricacies of the code we have developed, allowing us to swiftly address any issues and minimize downtime.
-    </p>
-    <br/>
-     <ul class="mt-2 text-md/relaxed text-gray-700">
-        <span class="font-bold"> Highlights: </span>
-        <li>✅ SEO: Search Engine Optimization</li>
-        <li>✅ Easy Management of Content</li>
-        <li>✅ Updating Website</li>
-    </ul>
-  </div>
-</article>
-    </div> */}
-    {/* <div class="flex justify-center pb-10">
-      <a
-          href=""
-        >
-        <span class=" rounded bg-teal-300 px-12 py-3 text-sm font-medium text-white transition hover:bg-teal-400 focus:outline-none focus:ring focus:ring-indigo-400">
-          Start Today
-        </span>
-        </a>
-    </div> */}
-{/* </section>
-</div>
-</div> */}
 </>
     );
 });
--- src/Landing.js
+++ src/Landing.js
@@ -2,79 +2,31 @@ import React, { useState, useEffect, useRef } from "react";
 import Cards from "./Cards";
 import About from "./About";
 import FooterCTA from "./FooterCTA";
-import { motion, useAnimation } from "framer-motion";
 import { Helmet } from 'react-helmet-async';
-import vid1 from './Images/navy.mp4';
-import vid2 from './Images/blue.mp4';
-import vid3 from './Images/red.mp4';
-import { ScrollSnapContainer, ScrollSnapSection } from 'react-scroll-snap';
+import vid1 from './Images/onevideo.mp4';
 import './App.css';
 
 const Landing = () => {
   const [currentTextIndex, setCurrentTextIndex] = useState(0);
-  const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
   const [textChange, setTextChange] = useState([
-    { className: "navy", content: "Minimalism.", videoIndex: 0 },
-    { className: "cornflower", content: "Flow.", videoIndex: 1 },
-    { className: "coral", content: "Impact.", videoIndex: 2 }
+    { className: "minimal", content: "Innovation."},
+    { className: "flow", content: "Impact."},
+    { className: "impact", content: "Results."},
   ]);
-
-  const videos = [vid1, vid2, vid3];
-  const videoCount = videos.length;
 
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentTextIndex(prevIndex => {
         const newIndex = (prevIndex + 1) % textChange.length;
-        setCurrentVideoIndex(textChange[newIndex].videoIndex);
         return newIndex;
       });
-    }, 10000);
+    }, 7000);
 
     return () => {
       clearInterval(interval);
     };
   }, [textChange.length]);
 
-  const handleVideoEnd = () => {
-    setCurrentTextIndex(prevIndex => {
-      const newIndex = (prevIndex + 1) % textChange.length;
-      setCurrentVideoIndex(textChange[newIndex].videoIndex);
-      return newIndex;
-    });
-  };
-
-  const cardsRef = useRef(null);
-  const aboutRef = useRef(null);
-  const footerRef = useRef(null);
-
-  const cardsControls = useAnimation();
-  const aboutControls = useAnimation();
-  const footerControls = useAnimation();
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollY = window.scrollY;
-      const windowHeight = window.innerHeight;
-
-      const cardsContainerTop = cardsRef.current?.offsetTop || 0;
-      const aboutContainerTop = aboutRef.current?.offsetTop || 0;
-      const footerContainerTop = footerRef.current?.offsetTop || 0;
-
-      const cardsVisible = scrollY > cardsContainerTop - windowHeight;
-      const aboutVisible = scrollY > aboutContainerTop - windowHeight;
-      const footerVisible = scrollY > footerContainerTop - windowHeight;
-
-      cardsControls.start({ opacity: cardsVisible ? 1 : 0.34 });
-      aboutControls.start({ opacity: aboutVisible ? 1 : 0.34 });
-      footerControls.start({ opacity: footerVisible ? 1 : 0.34 });
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, [cardsControls, aboutControls, footerControls]);
 
   return (
     <>
@@ -82,19 +34,18 @@ const Landing = () => {
         <title> Echo Web, LLC </title>
         <meta name='Landing Page' content='Call to actions and information about Echo Web, LLC.'/>
       </Helmet>
-      <div>
-        <section id="fade-in" className="overflow-hidden hadow-lg bg-white">
-          <video 
-          src={videos[currentVideoIndex]}
+      <div className="grid grid-cols-1">
+        <section id="fade-in" className="overflow flex justify-center items-center hadow-lg bg-white">
+          <video
+          src={vid1}
           autoPlay
           muted
-          onEnded={handleVideoEnd}
-          className="opacity-25 bg-cover object-cover w-full h-full fixed top-0 left-0 z-0"
+          className="opacity-100 bg-cover object-cover w-full h-full fixed top-0 left-0 z-0"
         />
           <div>
-            <div className="shadow-lg bg-gray-50 bg-opacity-75 rounded-md fade-in-comp mx-auto max-w-2xl text-center p-10 sm:p-32 lg:p-20">
-              <h1 className="flex justify-center lg:-mx-10 text-6xl lg:text-7xl font-bold text-center text-gray-900 opacity-100 drop-shadow-xl">
-                Equip Your Brand.
+            <div className="shadow-lg mb-40 hover:shadow-2xl bg-gray-50 bg-opacity-25 rounded-md fade-in-comp mx-auto max-w-2xl text-center p-10 sm:p-32 lg:p-20">
+              <h1 className="flex justify-center lg:-mx-10 text-6xl lg:text-7xl font-bold text-center text-gray-900 drop-shadow-xl">
+                Equip Your Brand
               </h1>
 
               <h2 className="font-bold pt-2 text-5xl lg:text-6xl">
@@ -103,9 +54,12 @@ const Landing = () => {
                 </span>
               </h2>
 
-              <p className="text-4xl text-center justify-centertext-white my-2 md:block drop-shadow-xl text-black font-bold p-2 rounded-md">
-                Less Clutter. <br/>
-                More Value.
+              <p className="text-4xl text-center justify-centertext-white my-2 md:block drop-shadow-xl text-gray-800 font-bold p-2 rounded-md">
+                Tested for Innovation
+                <br/>
+                Designed for Impact
+                <br/>
+                Engineered for Results
               </p>
 
               <div className="grid grid-cols-1 lg:grid-cols-2 justify-center opacity-100 gap-5">
@@ -132,35 +86,17 @@ const Landing = () => {
             </div>
           </div>
         </section>
-        <div className="-mt-24" ref={cardsRef}>
-          <motion.div
-            initial={{ opacity: 0.34, y: 100 }}
-            animate={cardsControls}
-            transition={{ duration: 1 }}
-          >
-            <Cards />
-          </motion.div>
+       
+       <div>
+            <Cards/>
         </div>
 
-        <div ref={aboutRef}>
-          <motion.div
-            initial={{ opacity: 0.34, y: 100 }}
-            animate={aboutControls}
-            transition={{ duration: 1 }}
-          >
-
-            <About />
-          </motion.div>
+        <div>
+            <About/>
         </div>
 
-        <div ref={footerRef}>
-          <motion.div
-            initial={{ opacity: 0.34, y: 100 }}
-            animate={footerControls}
-            transition={{ duration: 1 }}
-          >
-            <FooterCTA />
-          </motion.div>
+        <div>
+            <FooterCTA/>
         </div>
       </div>
     </>
